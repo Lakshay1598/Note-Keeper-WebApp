3cfcbcee4b3d49c5c24b052a524ca011f6728297
Lakshay Choube <96002691+Lakshay1598@users.noreply.github.com>
2023-02-23 23:00:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
-
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Note />
-      <Footer />
-    </>
-  );
-};
+import App from "./components/App";
 
 reactDom.render(
   <>
